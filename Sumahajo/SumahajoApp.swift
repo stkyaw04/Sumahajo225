d81ff2955fcf4d8a61af074666a0551daf0be3b6
stkyaw04 <137711211+stkyaw04@users.noreply.github.com>
2025-03-10 15:27:25 -0500

    Initial Commit

---
+++ Sumahajo/SumahajoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumahajoApp.swift
+//  Sumahajo
+//
+//  Created by Su Thiri Kyaw on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SumahajoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SumahajoUITests/SumahajoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SumahajoUITestsLaunchTests.swift
+//  SumahajoUITests
+//
+//  Created by Su Thiri Kyaw on 3/10/25.
+//
+
+import XCTest
+
+final class SumahajoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
