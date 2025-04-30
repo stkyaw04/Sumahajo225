732641933e437ded1837c946a27347b6b48becc3
HaroldPonce22 <hponcesa@macalester.edu>
2025-04-29 20:38:41 -0500

    Added The ability to use downloadable fonts
    Please test the fonts with this link . It didn't work on my computer although i downloaded and made sure everything worked but for some reason only the fonts in my font book (MacOS app) work.
    https://ngndang.itch.io/fat-pix-font

---
+++ Sumahajo/UI-FILES/FontHandler.swift
@@ -0,0 +1,15 @@
+//
+//  FontHandler.swift
+//  Sumahajo
+//
+//  Created by Harold Ponce on 4/29/25.
+// this allows the use of fonts when adding them in this file
+// if you dont understand this code watch this video https://www.youtube.com/watch?v=JB__8NzFadg "Add Custom Fonts to Your iOS App | SwiftUI"
+
+import SwiftUI
+
+extension Font {
+    static func pixelfont() -> Font{
+        return Font.custom("BalooBhaijaan-Regular", size: 32)
+    }
+}
---
+++ Sumahajo/UI-FILES/FontTestView.swift
@@ -0,0 +1,25 @@
+//
+//  FontTestView.swift
+//  Sumahajo
+//
+//  Created by Harold Ponce on 4/29/25.
+//
+import SwiftUI
+import AppKit
+
+struct FontTestView: View {
+    init() {
+        for family in NSFontManager.shared.availableFontFamilies {
+            print("Family: \(family)")
+        }
+    }
+    
+    
+    var body: some View {
+        Text("Hello with FatPix!")
+            .font(.custom("BalooBhaijaan-Regular", size: 32))
+    }
+    #Preview {
+        FontTestView()
+    }
+}
